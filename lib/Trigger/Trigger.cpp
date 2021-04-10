bef8c9ac3545d4786182bc1ec117a5fd5d574f6a
mrwolfjunior <emanuelebono97@gmail.com>
2021-04-10 18:26:32 +0200

    Added function for MQTT

--- lib/Trigger/Trigger.cpp
+++ lib/Trigger/Trigger.cpp
@@ -1,9 +1,11 @@
 #include <Trigger.h>
 
-Trigger::Trigger(Button * button, Light * light, String name) {
+Trigger::Trigger(Button * button, Light * light, String name, String id) {
 	this->button = button;
 	this->light = light;
 	this->name = name;
+	this->id = id;
+	MQTT_PREFIX = "homeassistant/light/" + id;
 	return;
 }
 
@@ -13,10 +15,45 @@ void Trigger::setup(void) {
 	return;
 }
 
+void Trigger::setup_mqtt(void) {
+	MQTT_CONFIG_TOPIC = MQTT_PREFIX + "/config";
+	MQTT_STATE_TOPIC = MQTT_PREFIX + "/state";
+	MQTT_COMMAND_TOPIC = MQTT_PREFIX + "/set";
+	MQTT_STATUS_TOPIC = MQTT_PREFIX + "/status";
+	return;
+}
+
 void Trigger::loop(void) {
 	if(button->isPressed() == true) {
 		light->triggerLight();
 	}
 
 	return;
+}
+
+void Trigger::setState(int newState) {
+	return light->setState(newState);
+}
+
+String Trigger::getName(void) {
+	return name;
+}
+String Trigger::getId(void) {
+	return id;
+}
+
+const char * Trigger::getMqttPrefix(void) {
+	return MQTT_PREFIX.c_str();
+}
+
+const char * Trigger::getMqttCongigTopic(void) {
+	return MQTT_CONFIG_TOPIC.c_str();
+}
+
+const char * Trigger::getMqttStateopic(void) {
+	return MQTT_STATE_TOPIC.c_str();
+}
+
+const char * Trigger::getMqttCommandTopic(void) {
+	return MQTT_COMMAND_TOPIC.c_str();
 }
--- lib/Trigger/Trigger.h
+++ lib/Trigger/Trigger.h
@@ -11,10 +11,24 @@ class Trigger
                 Button * button;
                 Light * light;
                 String name;
+                String id;
+                String MQTT_PREFIX;
+                String MQTT_CONFIG_TOPIC;
+                String MQTT_STATE_TOPIC;
+                String MQTT_COMMAND_TOPIC;
+                String MQTT_STATUS_TOPIC;
         public:
-                Trigger(Button * button, Light * light, String name);
+                Trigger(Button * button, Light * light, String name, String id);
                 void setup(void);
+                void setup_mqtt(void);
                 void loop(void);
+                void setState(int newState);
+                String getName(void);
+                String getId(void);
+                const char * getMqttPrefix(void);
+                const char * getMqttCongigTopic(void);
+                const char * getMqttStateopic(void);
+                const char * getMqttCommandTopic(void);
 };
 
 #endif
