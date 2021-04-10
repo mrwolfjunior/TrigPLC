dc34f9afe36ea55f18cbaf6498e2ce7bac6d3d3a
mrwolfjunior <emanuelebono97@gmail.com>
2021-04-10 22:34:20 +0200

    Changed reconnect logic

--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,9 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h> 
+#include <ArduinoJson.h>
 #include <config.h>
-#include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
+#include <semphr.h> // add the FreeRTOS functions for Semaphores (or Flags).
 
 #define DEBUG // uncomment to enable debug
 #define FLOOR 1
@@ -38,114 +38,122 @@
 SemaphoreHandle_t xSerialSemaphore;
 
 // FreeRTOS variable and function
-void TaskTrigger( void *pvParameters );
-void TaskIOT( void *pvParameters );
+void TaskTrigger(void *pvParameters);
+void TaskIOT(void *pvParameters);
 TaskHandle_t TaskIOTHandle; // handler for TaskIOT
 
 EthernetClient ethClient;
 // Ethernet variable
-byte ETH_MAC[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };   //physical mac address
-byte ETH_GATEWAY[] = { 192, 168, 88, 1 };                   // internet access via router
-byte ETH_SUBNET[] = { 255, 255, 255, 0 };                  //subnet mask
+byte ETH_MAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; //physical mac address
+byte ETH_GATEWAY[] = {192, 168, 88, 1};                // internet access via router
+byte ETH_SUBNET[] = {255, 255, 255, 0};                //subnet mask
 #if FLOOR == 0
-  byte ETH_IP[] = { 192, 168, 88, 15 };                      // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
+byte ETH_IP[] = {192, 168, 88, 15}; // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
 #elif FLOOR == 1
-  byte ETH_IP[] = { 192, 168, 88, 16 };                      // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
+byte ETH_IP[] = {192, 168, 88, 16}; // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
 #endif
 
 PubSubClient mqttClient(ethClient);
 long lastMQTTConnection = MQTT_CONNECTION_TIMEOUT;
 StaticJsonDocument<256> staticJsonDocument;
 char jsonBuffer[256];
+char stateOnJson[256];
+char stateOffJson[256];
 String MQTT_CONTROLLINO_PREFIX = "homeassistant/light/controllino_p" + FLOOR;
-const char * MQTT_CONTROLLINO_CONFIG_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/config").c_str();
-const char * MQTT_CONTROLLINO_STATE_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/state").c_str();
-const char * MQTT_CONTROLLINO_COMMAND_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/set").c_str();
-const char * MQTT_CONTROLLINO_STATUS_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/status").c_str();
+const char *MQTT_CONTROLLINO_CONFIG_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/config").c_str();
+const char *MQTT_CONTROLLINO_STATE_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/state").c_str();
+const char *MQTT_CONTROLLINO_COMMAND_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/set").c_str();
+const char *MQTT_CONTROLLINO_STATUS_TOPIC = String(MQTT_CONTROLLINO_PREFIX + "/status").c_str();
 
-void connectToMQTT();
-void publishToMQTT(const char* p_topic, char* p_payload);
-void subscribeToMQTT(const char* p_topic);
-void handleMQTTMessage(char* p_topic, byte* p_payload, unsigned int p_length);
+void publishToMQTT(const char *p_topic, char *p_payload);
+void subscribeToMQTT(const char *p_topic);
+void handleMQTTMessage(char *p_topic, byte *p_payload, unsigned int p_length);
 
 #if FLOOR == O
-  Trigger triggers[] = {
-    Trigger(&button_A0, &light_R0, "Lavanderia", "p0_lavanderia"), // Lavanderia
+Trigger triggers[] = {
+    Trigger(&button_A0, &light_R0, "Lavanderia", "p0_lavanderia"),   // Lavanderia
     Trigger(&button_A2, &light_R1, "Laboratorio", "p0_laboratorio"), // Locale tecnico - Ipotizzato ingresso
-    Trigger(&button_A1, &light_R2, "Garage", "p0_garage"), // Salone
-    Trigger(&button_A4, &light_R3, "Magazzino", "p0_magazzino"), // Garage
-    Trigger(&button_A3, &light_R4, "Ingresso", "p0_ingresso"), // Est. Porta
-    Trigger(&button_A5, &light_R5, "Gradini", "p0_gradini") // Gradini
-  }; // Luci esterne su A3
+    Trigger(&button_A1, &light_R2, "Garage", "p0_garage"),           // Salone
+    Trigger(&button_A4, &light_R3, "Magazzino", "p0_magazzino"),     // Garage
+    Trigger(&button_A3, &light_R4, "Ingresso", "p0_ingresso"),       // Est. Porta
+    Trigger(&button_A5, &light_R5, "Gradini", "p0_gradini")          // Gradini
+};                                                                   // Luci esterne su A3
 #elif FLOOR == 1
-  Trigger triggers[] = {
-    Trigger(&button_A5, &light_R0, "Ballatoio", "p1_ballatoio"), // Ballatoio
-    Trigger(&button_A14, &light_R1, "Esterno sala", "p1_est_sala"), // Ext nord
-    Trigger(&button_A13, &light_R2, "Esterno cucina", "p1_est_cucina"), // Ext cucina
-    Trigger(&button_A4, &light_R3, "Stanza Ema", "p1_stanza_ema"), // Stanza 2 -- Ema
-    Trigger(&button_A12, &light_R4, "Cucina", "p1_cucina"), // Cucina
-    Trigger(&button_A8, &light_R5, "Stanza Gio", "p1_stanza_gio"), // Stanza 3 -- Gio
-    Trigger(&button_A2, &light_R6, "Corridoio", "p1_corridoio"), // Corridoio
-    Trigger(&button_A9, &light_R5, "Esterno Gio", "p1_est_gio"), // Esterno 3 -- Gio --> default R7
-    Trigger(&button_A6, &light_R8, "Lavanderia", "p1_lavanderia"), // Lavanderia
-    Trigger(&button_A7, &light_R9, "Bagno piccolo", "p1_bagno_piccolo"), // Bagno piccolo
-    Trigger(&button_A11, &light_R10, "Sala", "p1_sala"), // Sala
-    Trigger(&button_A3, &light_R3, "Esterno Ema", "p1_est_ema"), // Esterno 2 - Ema --> default R11
+Trigger triggers[] = {
+    Trigger(&button_A5, &light_R0, "Ballatoio", "p1_ballatoio"),                 // Ballatoio
+    Trigger(&button_A14, &light_R1, "Esterno sala", "p1_est_sala"),              // Ext nord
+    Trigger(&button_A13, &light_R2, "Esterno cucina", "p1_est_cucina"),          // Ext cucina
+    Trigger(&button_A4, &light_R3, "Stanza Ema", "p1_stanza_ema"),               // Stanza 2 -- Ema
+    Trigger(&button_A12, &light_R4, "Cucina", "p1_cucina"),                      // Cucina
+    Trigger(&button_A8, &light_R5, "Stanza Gio", "p1_stanza_gio"),               // Stanza 3 -- Gio
+    Trigger(&button_A2, &light_R6, "Corridoio", "p1_corridoio"),                 // Corridoio
+    Trigger(&button_A9, &light_R5, "Esterno Gio", "p1_est_gio"),                 // Esterno 3 -- Gio --> default R7
+    Trigger(&button_A6, &light_R8, "Lavanderia", "p1_lavanderia"),               // Lavanderia
+    Trigger(&button_A7, &light_R9, "Bagno piccolo", "p1_bagno_piccolo"),         // Bagno piccolo
+    Trigger(&button_A11, &light_R10, "Sala", "p1_sala"),                         // Sala
+    Trigger(&button_A3, &light_R3, "Esterno Ema", "p1_est_ema"),                 // Esterno 2 - Ema --> default R11
     Trigger(&button_A1, &light_R12, "Esterno camera da letto", "p1_est_camera"), // Esterno 1
-    Trigger(&button_A10, &light_R13, "Bagno", "p1_bagno"), // Bagno
-    Trigger(&button_A0, &light_R14, "Camera da letto", "p1_camera") // Stanza 1
-  };
+    Trigger(&button_A10, &light_R13, "Bagno", "p1_bagno"),                       // Bagno
+    Trigger(&button_A0, &light_R14, "Camera da letto", "p1_camera")              // Stanza 1
+};
 #endif
 
-
-void setup() {
+void setup()
+{
 
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  
-  while (!Serial) {
+
+  while (!Serial)
+  {
     ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
   }
 
   // Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
   // because it is sharing a resource, such as the Serial port.
   // Semaphores should only be used whilst the scheduler is running, but we can set it up here.
-  if ( xSerialSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  if (xSerialSemaphore == NULL) // Check to confirm that the Serial Semaphore has not already been created.
   {
-    xSerialSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
-    if ( ( xSerialSemaphore ) != NULL )
-      xSemaphoreGive( ( xSerialSemaphore ) );  // Make the Serial Port available for use, by "Giving" the Semaphore.
+    xSerialSemaphore = xSemaphoreCreateMutex(); // Create a mutex semaphore we will use to manage the Serial Port
+    if ((xSerialSemaphore) != NULL)
+      xSemaphoreGive((xSerialSemaphore)); // Make the Serial Port available for use, by "Giving" the Semaphore.
   }
 
+  xTaskCreate(
+      TaskTrigger, "Trigger" // A name just for humans
+      ,
+      128 // This stack size can be checked & adjusted by reading the Stack Highwater
+      ,
+      NULL, 3 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+      ,
+      NULL);
 
-   xTaskCreate(
-    TaskTrigger
-    ,  "Trigger"  // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
-
-    xTaskCreate(
-    TaskIOT
-    ,  "IOT"  // A name just for humans
-    ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  &TaskIOTHandle );
+  xTaskCreate(
+      TaskIOT, "IOT" // A name just for humans
+      ,
+      1024 // This stack size can be checked & adjusted by reading the Stack Highwater
+      ,
+      NULL, 1 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+      ,
+      &TaskIOTHandle);
 }
 
-void loop() {
+void loop()
+{
 }
 
-void TaskTrigger( void *pvParameters __attribute__((unused)) ) {
+void TaskTrigger(void *pvParameters __attribute__((unused)))
+{
   // initialize in pin
-  for(auto &item : triggers) {
+  for (auto &item : triggers)
+  {
     item.setup();
   }
 
-  for (;;) {
-    for(auto &item : triggers) {
+  for (;;)
+  {
+    for (auto &item : triggers)
+    {
       item.loop();
     }
 
@@ -153,34 +161,36 @@ void TaskTrigger( void *pvParameters __attribute__((unused)) ) {
   }
 }
 
-void TaskIOT( void *pvParameters __attribute__((unused)) ) {
-  
+void TaskIOT(void *pvParameters __attribute__((unused)))
+{
+
   Ethernet.begin(ETH_MAC, ETH_IP, ETH_GATEWAY, ETH_SUBNET);
   vTaskDelay(1);
 
   // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+  if (Ethernet.hardwareStatus() == EthernetNoHardware)
+  {
     vTaskDelete(TaskIOTHandle); // Ethernet shield not found, delete task
   }
-  
-  while (Ethernet.linkStatus() == LinkOFF) {
-    #ifdef DEBUG
-      if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-      {
-        Serial.println("ERROR: No ethernet cable connected, wait");
-        xSemaphoreGive( xSerialSemaphore );
-      }
-    #endif
+
+  while (Ethernet.linkStatus() == LinkOFF)
+  {
+#ifdef DEBUG
+    if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+    {
+      Serial.println("ERROR: No ethernet cable connected, wait");
+      xSemaphoreGive(xSerialSemaphore);
+    }
+#endif
 
     vTaskDelay(60000 / portTICK_PERIOD_MS); // Ethernet cable is not connected. Wait until cable is connected
   }
 
-  for(auto &item : triggers) {
+  for (auto &item : triggers)
+  {
     item.setup_mqtt();
   }
 
-  char stateOnJson[256];
-  char stateOffJson[256];
   staticJsonDocument["state"] = MQTT_STATE_ON_PAYLOAD;
   serializeJson(staticJsonDocument, stateOnJson);
   staticJsonDocument["state"] = MQTT_STATE_OFF_PAYLOAD;
@@ -191,11 +201,42 @@ void TaskIOT( void *pvParameters __attribute__((unused)) ) {
   mqttClient.setServer(MQTT_SERVER, MQTT_SERVER_PORT);
   mqttClient.setCallback(handleMQTTMessage);
 
-  for (;;) {
-    connectToMQTT();
+  for (;;)
+  {
+    if (!mqttClient.connected())
+    {
+      if (lastMQTTConnection + MQTT_CONNECTION_TIMEOUT > xTaskGetTickCount())
+      {
+        if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD, MQTT_CONTROLLINO_CONFIG_TOPIC, 0, 1, "dead"))
+        {
 
-    for(auto &item : triggers) {
-      if(item.isChanged()) {
+          mqttClient.publish(MQTT_CONTROLLINO_CONFIG_TOPIC, "alive", true);
+
+          for (auto &item : triggers)
+          {
+            // MQTT discovery for Home Assistant
+            publishToMQTT(item.getMqttConfigTopic(), item.getJsonConfig());
+            subscribeToMQTT(item.getMqttCommandTopic());
+          }
+        }
+        else
+        {
+#ifdef DEBUG
+          if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+          {
+            Serial.println("ERROR: The connection to the MQTT broker failed");
+            xSemaphoreGive(xSerialSemaphore);
+          }
+#endif
+        }
+        lastMQTTConnection = xTaskGetTickCount();
+      }
+    }
+
+    for (auto &item : triggers)
+    {
+      if (item.isChanged())
+      {
         switch (item.getState())
         {
         case LOW:
@@ -214,134 +255,114 @@ void TaskIOT( void *pvParameters __attribute__((unused)) ) {
   }
 }
 
-
-void connectToMQTT() {
-  if (!mqttClient.connected()) {
-    if (lastMQTTConnection + MQTT_CONNECTION_TIMEOUT < xTaskGetTickCount()) {
-      if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD, MQTT_CONTROLLINO_CONFIG_TOPIC, 0, 1, "dead")) {
-
-        mqttClient.publish(MQTT_CONTROLLINO_CONFIG_TOPIC, "alive", true);
-
-        staticJsonDocument["cmd_t"] = "~/set";
-        staticJsonDocument["stat_t"] = "~/state";
-        staticJsonDocument["schema"] = "json";
-
-        for(auto &item : triggers) {
-          // MQTT discovery for Home Assistant
-          staticJsonDocument["~"] = item.getMqttPrefix();
-          staticJsonDocument["name"] = item.getName();
-          staticJsonDocument["unique_id"] = item.getId();
-          serializeJson(staticJsonDocument, jsonBuffer);
-          publishToMQTT(item.getMqttCongigTopic(), jsonBuffer);
-
-          subscribeToMQTT(item.getMqttCommandTopic());
-        } 
-      }
-      else {
-        #ifdef DEBUG
-          if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-          {
-            Serial.println("ERROR: The connection to the MQTT broker failed");
-            xSemaphoreGive( xSerialSemaphore );
-          }
-        #endif
-      }
-      lastMQTTConnection = xTaskGetTickCount();
+void publishToMQTT(const char *p_topic, char *p_payload)
+{
+  if (mqttClient.publish(p_topic, p_payload, true))
+  {
+#ifdef DEBUG
+    if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+    {
+      Serial.println("INFO: MQTT message published successfully, topic & payload: ");
+      Serial.println(p_topic);
+      Serial.println(p_payload);
+      xSemaphoreGive(xSerialSemaphore);
     }
+#endif
   }
-}
-
-void publishToMQTT(const char* p_topic, char* p_payload) {
-  if (mqttClient.publish(p_topic, p_payload, true)) {
-    #ifdef DEBUG
-      if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-      {
-        Serial.println("INFO: MQTT message published successfully, topic & payload: ");
-        Serial.println(p_topic);
-        Serial.println(p_payload);
-        xSemaphoreGive( xSerialSemaphore );
-      }
-    #endif
-  } else {
-    #ifdef DEBUG
-      if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-      {
-        Serial.println("ERROR: MQTT message not published, either connection lost, or message too large. Topic & payload: ");
-        Serial.println(p_topic);
-        Serial.println(p_payload);
-        xSemaphoreGive( xSerialSemaphore );
-      }
-    #endif
+  else
+  {
+#ifdef DEBUG
+    if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+    {
+      Serial.println("ERROR: MQTT message not published, either connection lost, or message too large. Topic & payload: ");
+      Serial.println(p_topic);
+      Serial.println(p_payload);
+      xSemaphoreGive(xSerialSemaphore);
+    }
+#endif
   }
 
   return;
 }
 
-void subscribeToMQTT(const char* p_topic) {
-  if (mqttClient.subscribe(p_topic)) {
-    #ifdef DEBUG
-      if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-      {
-        Serial.println("INFO: Sending the MQTT subscribe succeeded for topic: ");
-        Serial.println(p_topic);
-        xSemaphoreGive( xSerialSemaphore );
-      }
-    #endif
-  } else {
-    #ifdef DEBUG
-      if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-      {
-        Serial.println("ERROR: Sending the MQTT subscribe failed for topic: ");
-        Serial.println(p_topic);
-        xSemaphoreGive( xSerialSemaphore );
-      }
-    #endif
+void subscribeToMQTT(const char *p_topic)
+{
+  if (mqttClient.subscribe(p_topic))
+  {
+#ifdef DEBUG
+    if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+    {
+      Serial.println("INFO: Sending the MQTT subscribe succeeded for topic: ");
+      Serial.println(p_topic);
+      xSemaphoreGive(xSerialSemaphore);
+    }
+#endif
+  }
+  else
+  {
+#ifdef DEBUG
+    if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+    {
+      Serial.println("ERROR: Sending the MQTT subscribe failed for topic: ");
+      Serial.println(p_topic);
+      xSemaphoreGive(xSerialSemaphore);
+    }
+#endif
   }
 }
 
-void handleMQTTMessage(char* p_topic, byte* p_payload, unsigned int p_length) {
+void handleMQTTMessage(char *p_topic, byte *p_payload, unsigned int p_length)
+{
   // light_R1
 
   // concatenates the payload into a string
   String payload;
-  for (uint8_t i = 0; i < p_length; i++) {
+  for (uint8_t i = 0; i < p_length; i++)
+  {
     payload.concat((char)p_payload[i]);
   }
 
-  #ifdef DEBUG
-    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
+#ifdef DEBUG
+  if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
+  {
+    Serial.println("INFO: New MQTT message received, topic:");
+    Serial.println(p_topic);
+    Serial.println("INFO: MQTT payload:");
+    Serial.println(payload);
+
+    xSemaphoreGive(xSerialSemaphore);
+  }
+#endif
+
+  for (auto &item : triggers)
+  {
+    if (String(item.getMqttCommandTopic()).equals(p_topic))
     {
-      Serial.println("INFO: New MQTT message received, topic:");
-      Serial.println(p_topic);
-      Serial.println("INFO: MQTT payload:");
-      Serial.println(payload);
-
-      xSemaphoreGive( xSerialSemaphore );
-    }
-  #endif
-
-  for(auto &item : triggers) {
-    if(String(item.getMqttCommandTopic()).equals(p_topic)) {
       DynamicJsonDocument doc(1024);
       auto error = deserializeJson(doc, p_payload);
-      if (error) {
+      if (error)
+      {
 
-        #ifdef DEBUG
-        if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
+#ifdef DEBUG
+        if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE)
         {
           Serial.println("deserializeJson() failed with code: ");
           Serial.println(error.c_str());
 
-          xSemaphoreGive( xSerialSemaphore );
+          xSemaphoreGive(xSerialSemaphore);
         }
-        #endif
+#endif
         return;
       }
 
-      if (doc.containsKey("state")) {
-        if (strcmp(doc["state"], MQTT_STATE_ON_PAYLOAD) == 0) {
+      if (doc.containsKey("state"))
+      {
+        if (strcmp(doc["state"], MQTT_STATE_ON_PAYLOAD) == 0)
+        {
           item.setState(HIGH);
-        } else if (strcmp(doc["state"], MQTT_STATE_OFF_PAYLOAD) == 0) {
+        }
+        else if (strcmp(doc["state"], MQTT_STATE_OFF_PAYLOAD) == 0)
+        {
           item.setState(LOW);
         }
         serializeJson(doc, jsonBuffer);
