c9578b8ed4dfebc1cdbac87ad80ba8b8591f0c94
mrwolfjunior <emanuelebono97@gmail.com>
2021-04-11 15:03:08 +0200

    Reduced allocated space by class

--- lib/Trigger/Trigger.cpp
+++ lib/Trigger/Trigger.cpp
@@ -5,7 +5,6 @@ Trigger::Trigger(Button * button, Light * light, String name, String id) {
 	this->light = light;
 	this->name = name;
 	this->id = id;
-	MQTT_PREFIX = "homeassistant/light/" + id;
 	isChangedFlag = false;
 	return;
 }
@@ -13,14 +12,6 @@ Trigger::Trigger(Button * button, Light * light, String name, String id) {
 void Trigger::setup(void) {
 	button->setup();
 	light->setup();
-	return;
-}
-
-void Trigger::setup_mqtt(void) {
-	MQTT_CONFIG_TOPIC = MQTT_PREFIX + "/config";
-	MQTT_STATE_TOPIC = MQTT_PREFIX + "/state";
-	MQTT_COMMAND_TOPIC = MQTT_PREFIX + "/set";
-	MQTT_STATUS_TOPIC = MQTT_PREFIX + "/status";
 	return;
 }
 
@@ -54,20 +45,4 @@ String Trigger::getName(void) {
 }
 String Trigger::getId(void) {
 	return id;
-}
-
-const char * Trigger::getMqttPrefix(void) {
-	return MQTT_PREFIX.c_str();
-}
-
-const char * Trigger::getMqttConfigTopic(void) {
-	return MQTT_CONFIG_TOPIC.c_str();
-}
-
-const char * Trigger::getMqttStateopic(void) {
-	return MQTT_STATE_TOPIC.c_str();
-}
-
-const char * Trigger::getMqttCommandTopic(void) {
-	return MQTT_COMMAND_TOPIC.c_str();
 }
